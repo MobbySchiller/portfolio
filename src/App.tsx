5bfb71eb694df45cf59896f538be7e8345df747a
MobbySchiller <kb.burzynski@gmail.com>
2022-11-20 21:28:20 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import './App.css'
+const App: React.FC = () => {
+  const [isLoaded, setIsLoaded] = useState<boolean>(false)
+
+  return (
+    <div className="wrapper">
+
+    </div>
+  )
+}
+
+export default App
