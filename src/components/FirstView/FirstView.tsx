1ab4d64bb51ffbed5265b9d7ccc9a9dc9d4f1f29
MobbySchiller <kb.burzynski@gmail.com>
2022-11-21 23:06:26 +0100

    Add FirstView component and change nav color

--- src/components/FirstView/FirstView.tsx
+++ src/components/FirstView/FirstView.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import './FirstView.scss'
 
 const FirstView: React.FC = () => {
     return (
-        <div className='first-view'>
-            <div className='intro'>
+        <div
+            className='first-view'>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }} className='intro'>
                 <span className='intro__hello'>Hello, my name is</span>
                 <h1 className='intro__name'>Krzysztof Burzyński.</h1>
                 <span className='intro__occupation'>Beginner front-end developer.</span>
-            </div>
+                <p className='intro__description'>Welcome on my website. This is the place where you will find information about me and my path to becoming a front-end developer.</p>
+            </motion.div>
         </div>
     )
 }
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { motion } from 'framer-motion'
 import { State } from '../Header/Header'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
@@ -7,8 +8,29 @@ import './Nav.scss'
 
 const Nav: React.FC<{ state: State }> = ({ state }) => {
     const { navIsActive, setNavIsActive } = state
-
     const handleHamburger = (): void => setNavIsActive(!navIsActive)
+
+    const sections = ['about', 'projects', 'contact', 'resume']
+
+    const framerList = {
+        visible: {
+            transition: { staggerChildren: .1 }
+        }
+    }
+
+    const framerElement = {
+        visible: { opacity: 1, y: 0 },
+        hidden: { opacity: 0, y: '100%' },
+        whileHover: { scale: 1.2 }
+    }
+
+    const navElements = sections.map(section => (
+        <motion.li
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.8 }}
+            variants={framerElement}
+            className='nav__element'><a href={`#${section}`} className='nav__link'>{section}</a></motion.li>
+    ))
 
     return (
         <nav>
@@ -19,12 +41,13 @@ const Nav: React.FC<{ state: State }> = ({ state }) => {
                     <FontAwesomeIcon icon={faBars} className='hamburger__bars' />}
             </div>
             {navIsActive &&
-                <ul className='nav__list'>
-                    <li className='nav__element'><a href="#about" className='nav__link'>about</a></li>
-                    <li className='nav__element'><a href="#projects" className='nav__link'>projects</a></li>
-                    <li className='nav__element'><a href="#contact" className='nav__link'>contact</a></li>
-                    <li className='nav__element'><a href="#resume" className='nav__link'>resume</a></li>
-                </ul>
+                <motion.ul
+                    variants={framerList}
+                    initial='hidden'
+                    animate='visible'
+                    className='nav__list'>
+                    {navElements}
+                </motion.ul>
             }
         </nav>
     )
