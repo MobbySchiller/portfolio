fd0caeb2c8ac1a1e5f655b4b52ca9e9bfff90953
MobbySchiller <kb.burzynski@gmail.com>
2022-12-07 16:57:17 +0100

    Change header behaviour on scrolling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useLayoutEffect, useEffect } from 'react'
 import Nav from '../Nav/Nav'
 import './Header.scss'
 
@@ -12,19 +12,22 @@ export interface State {
 }
 
 const Header: React.FC = () => {
+    const [isHeaderVisible, setIsHeaderVisible] = useState<boolean>(true)
     const [navIsActive, setNavIsActive] = useState<boolean>(false)
     const logo = '<kb/>'
 
+    window.addEventListener('scroll', () => setIsHeaderVisible(window.scrollY < 400))
+
     window.addEventListener('resize', () => window.innerWidth >= Breakpoint.Smartphone ? setNavIsActive(true) : setNavIsActive(false))
 
-    useEffect(() => {
+    useLayoutEffect(() => {
         window.innerWidth >= Breakpoint.Smartphone ? setNavIsActive(true) : setNavIsActive(false)
     }, [])
 
-
     return (
         <header
-            className={navIsActive ? 'header header--active-nav' : 'header'}>
+            style={isHeaderVisible ? { top: 0 } : { top: '-60px' }}
+            className={`header ${navIsActive ? 'header--active-nav' : ''}`}>
             <div className='logo'>
                 <a href='/' className='logo__sign'>{logo}</a>
             </div>
