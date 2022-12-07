1e947f1cdd557446a678f17a089e730412bbcc74
MobbySchiller <kb.burzynski@gmail.com>
2022-12-07 23:58:18 +0100

    Add TechnologiesCarousel component

---
+++ src/components/TechnologiesCarousel/TechnologiesCarousel.tsx
@@ -0,0 +1,68 @@
+import { FC } from 'react'
+import './TechnologiesCarousel.scss'
+
+const TechnologiesCarousel: FC = () => {
+
+    return (
+        <div className="slider">
+            <div className="slide-track">
+                <div className="slide">
+                    <img src='../../../assets/html5.svg' alt='HTML5' />
+                </div>
+                <div className="slide">
+                    <img src='../../../assets/css3.svg' alt='CSS3' />
+                </div>
+                <div className="slide">
+                    <img src='../../../assets/javascript.svg' alt='Javascript' />
+                </div>
+                <div className="slide">
+                    <img src='../../../assets/typescript.svg' alt='Typescript' />
+                </div>
+                <div className="slide">
+                    <img src='../../../assets/react.svg' alt='React' />
+                </div>
+                <div className="slide">
+                    <img src='../../../assets/sass.svg' alt='Sass' />
+                </div>
+                <div className="slide">
+                    <img src='../../../assets/npm.svg' alt='NPM' />
+                </div>
+                <div className="slide">
+                    <img src='../../../assets/mui.svg' alt='Material UI' />
+                </div>
+                <div className="slide">
+                    <img src='../../../assets/webpack.svg' alt='Webpack' />
+                </div>
+                <div className="slide">
+                    <img src='../../../assets/html5.svg' alt='HTML5' />
+                </div>
+                <div className="slide">
+                    <img src='../../../assets/css3.svg' alt='CSS3' />
+                </div>
+                <div className="slide">
+                    <img src='../../../assets/javascript.svg' alt='Javascript' />
+                </div>
+                <div className="slide">
+                    <img src='../../../assets/typescript.svg' alt='Typescript' />
+                </div>
+                <div className="slide">
+                    <img src='../../../assets/react.svg' alt='React' />
+                </div>
+                <div className="slide">
+                    <img src='../../../assets/sass.svg' alt='Sass' />
+                </div>
+                <div className="slide">
+                    <img src='../../../assets/npm.svg' alt='NPM' />
+                </div>
+                <div className="slide">
+                    <img src='../../../assets/mui.svg' alt='Material UI' />
+                </div>
+                <div className="slide">
+                    <img src='../../../assets/webpack.svg' alt='Webpack' />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TechnologiesCarousel
